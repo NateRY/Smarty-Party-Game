dc1b7edbdb305d9aef9127c259100d896fc29259
n <nathaniel.ryan978@gmail.com>
2026-05-16 17:32:04 -0400

    lets see if this work

---
+++ src/main/java/Controller/homeScreenController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+//This is the Controller class for the "Main Menu"
+public class homeScreenController {
+
+    //This brings player to the name entry scene
+    public void goP1_NewGame() throws IOException {
+        App.setRoot("P1_Gameplay_Entry");
+    }
+
+    //Brings player to 2 player game mode
+    public void goP2_NewGame() throws IOException {
+        App.setRoot("P2_Gamplay_Entry");
+    }
+
+    //Brings player to the Question entry mode, so player can
+    //sumbit a questions to the database
+    public void goQuestionEntry() throws IOException {
+        App.setRoot("Question_Entry_Scene");
+    }
+
+
+    //Brings player to the leaderboard, send quary to database
+    public void goLeaderboard() throws IOException {
+        App.setRoot("leaderboard");
+    }
+
+    //makes sure that the "Main Menu" button leads to the start menu
+    @FXML
+    private void goHome() throws IOException {
+        App.setRoot("Smarty_Party_HomeScreen");
+
+    }
+
+    //Connected to the quit button, close the program
+    public void closeGame() throws IOException {
+        Platform.exit();
+    }
+
+
+}
