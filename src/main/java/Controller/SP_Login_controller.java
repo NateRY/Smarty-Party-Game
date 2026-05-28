e58ae91f9f99445470e154454b2029d7d9532a54
Nathaniel Ryan <85573981+NateRY@users.noreply.github.com>
2026-05-28 12:40:24 -0400

    Still working on Login. I changed the text field that has the password to keep the user password hidden. Running into a problem with the login verification. Trying to fix next.
    In addition, added a way to add player account to database.

--- src/main/java/Controller/SP_Login_controller.java
+++ src/main/java/Controller/SP_Login_controller.java
@@ -1,9 +1,12 @@
 package Controller;
 
+import database.PlayerDatabaseManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
@@ -14,7 +17,7 @@ public class SP_Login_controller {
     private Boolean logStatues = false;
 
     @FXML private TextField usernameField;
-    @FXML private TextField passwordField;
+    @FXML private PasswordField pswField;
     @FXML private Button EnterButton;
     @FXML private Label loginFeedbackLabel;
 
@@ -22,32 +25,78 @@ public class SP_Login_controller {
     public void initialize() {
         //put link to database inside this method
 
-
-
         username = usernameField.getText().trim();
-        password = passwordField.getText().trim();
+        password = pswField.getText().trim();
 
-
-        logStatues = false;
-
-
-
-
-
-
-
-        LoginCheck();
 
     }
 
 
-    public void LoginCheck() {
+   @FXML public void LoginCheck() {
         //if return 'logStatues' is true then the visibility will turn
         //to a success
 
-        loginFeedbackLabel.setVisible(logStatues);
+        if (usernameField.getText().equals("") || pswField.getText().equals("")) {
+            loginFeedbackLabel.setText("Please fill all the fields");
+            loginFeedbackLabel.setTextFill(Color.RED);
+            loginFeedbackLabel.setVisible(true);
+
+            System.out.println("Fill all the fields");
+
+        }
+
+
+        logStatues = PlayerDatabaseManager.verifylogin(username, password);
+
+        if(!logStatues) {
+
+            loginFeedbackLabel.setText("Login Failed! Wrong Username or Password");
+            loginFeedbackLabel.setTextFill(Color.RED);
+            loginFeedbackLabel.setVisible(true);
+
+        }else if(logStatues)
+        {
+            loginFeedbackLabel.setTextFill(Color.GREEN);
+            loginFeedbackLabel.setText("Loginjjjjjjj Successfully");
+            loginFeedbackLabel.setVisible(true);
+        }
+
+
+
+
 
     }
+
+
+    public void CreateNewPlayer() {
+
+        if (usernameField.getText().equals("") || pswField.getText().equals("")) {
+            loginFeedbackLabel.setText("Please fill all the fields");
+            loginFeedbackLabel.setVisible(true);
+            loginFeedbackLabel.setTextFill(Color.RED);
+
+            System.out.println("Fill all the fields");
+        }
+
+        PlayerDatabaseManager.CreatePlayer(username,password);
+
+        if(logStatues == false) {
+
+            loginFeedbackLabel.setVisible(logStatues);
+        }else
+        {
+            loginFeedbackLabel.setTextFill(Color.GREEN);
+            loginFeedbackLabel.setText("Login Successfully");
+            loginFeedbackLabel.setVisible(true);
+
+        }
+
+
+    }
+
+
+
+
 
 
     @FXML public void goHome() throws IOException {
--- src/main/java/database/PlayerDatabaseManager.java
+++ src/main/java/database/PlayerDatabaseManager.java
@@ -1,4 +1,75 @@
 package database;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class PlayerDatabaseManager {
+    //Database Access Object
+
+
+    public static boolean verifylogin(String username, String password) {
+
+        String loginStatment = "SELECT * FROM player WHERE username = ? AND password = ?";
+
+
+        try{
+            Connection conn = DatabaseManager.connect();
+
+            PreparedStatement statement = conn.prepareStatement(loginStatment);
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.next();
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+
+    public static boolean CreatePlayer(String username, String password) {
+
+        if(verifylogin(username, password)){
+            return false;
+        }
+        String loginStatment = "INSERT INTO player (username, password) VALUES (?, ?)";
+
+
+        try{
+            Connection connect = DatabaseManager.connect();
+
+            PreparedStatement statement = connect.prepareStatement(loginStatment);
+
+            statement.setString(1, username);
+            statement.setString(2, username);
+
+            int row =  statement.executeUpdate();
+
+            if(row > 0){
+                System.out.println("Successfully Created Player");
+                return true;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+
 }
